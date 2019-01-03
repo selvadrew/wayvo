a1c74dcde1d75d2c833807068007611fd22d4f61
selvadrew <andrew.selvadurai6@gmail.com>
2019-01-03 16:07:22 -0500

    before photo upload update

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,9 +9,10 @@
 // to generate the release APK
 // $ cd android
 // $ ./gradlew assembleRelease
+// android/app/build/outputs/apk/release
 
 // https://sheltered-escarpment-63295.herokuapp.com/
 
-//export const HOST = "http://192.168.1.182:3000";
+export const HOST = "http://192.168.1.143:3000";
 
-export const HOST = "https://sheltered-escarpment-63295.herokuapp.com/";
+//export const HOST = "https://sheltered-escarpment-63295.herokuapp.com/";
